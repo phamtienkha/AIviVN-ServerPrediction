267cb97b2ebdb0123eca08b71328ee37659165bd
phamtienkha <phamtienkha@gmail.com>
2019-06-10 16:42:14 +0700

    Update Loss.py

--- Loss.py
+++ Loss.py
@@ -12,11 +12,3 @@ def MAPE(y_true, y_pred):
     for i in range(len(y_true)):
         loss += 100 * abs(y_true[i] - y_pred[i]) / (abs(y_true[i]) + 1e-6)
     return loss / len(y_true)
-
-
-def signMAPE(y_true, y_pred):
-    assert len(y_true) == len(y_pred)
-    loss = 0
-    for i in range(len(y_true)):
-        loss += 100 * (y_true[i] - y_pred[i]) / (abs(y_true[i]) + 1e-6)
-    return loss / len(y_true)
