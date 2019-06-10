eef2b40e60e54cc509302e94e7ff9860aaedbe4a
phamtienkha <phamtienkha@gmail.com>
2019-06-10 16:39:05 +0700

    Add files via upload

---
+++ Loss.py
@@ -0,0 +1,22 @@
+def sMAPE(y_true, y_pred):
+    assert len(y_true) == len(y_pred)
+    loss = 0
+    for i in range(len(y_true)):
+        loss += 200 * abs(y_true[i] - y_pred[i]) / (abs(y_true[i]) + abs(y_pred[i]))
+    return loss / len(y_true)
+
+
+def MAPE(y_true, y_pred):
+    assert len(y_true) == len(y_pred)
+    loss = 0
+    for i in range(len(y_true)):
+        loss += 100 * abs(y_true[i] - y_pred[i]) / (abs(y_true[i]) + 1e-6)
+    return loss / len(y_true)
+
+
+def signMAPE(y_true, y_pred):
+    assert len(y_true) == len(y_pred)
+    loss = 0
+    for i in range(len(y_true)):
+        loss += 100 * (y_true[i] - y_pred[i]) / (abs(y_true[i]) + 1e-6)
+    return loss / len(y_true)
